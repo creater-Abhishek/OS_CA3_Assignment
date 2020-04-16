e4d46b1917e6611d4f2db4a4a5068cf92ee9579c
Abhishek Yadav <54737348+creater-Abhishek@users.noreply.github.com>
2020-04-16 14:32:07 +0530

    Update CA_48_K18NS.c

--- CA_48_K18NS.c
+++ CA_48_K18NS.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 struct stud
 {
-	//student id
-    int Sid;
-    //taken time
-    int Food_time;
-    //For waiting time
-    int WT;
-    // for Turnaround time
-    int TAtime;
-};
-
-void accept(struct stud list[], int s);
-void display(struct stud list[], int s);
-void scheduling(struct stud list[], int s);
-void waitingTime(struct stud list[], int n);
-void turnAroundTime(struct stud list[], int n);
-#include <stdio.h>
-struct stud
-{
-	//student id
+    //student id
     int Sid;
     //taken time
     int Food_time;
